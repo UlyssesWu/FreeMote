98d5bc2fb50e1a836d7c6b8faf6ad56160d2538c
UlyssesWu <wdwxy12345@gmail.com>
2020-05-31 17:38:32 +0800

    Fix win DXT5 texture spilt issue

--- FreeMote.Psb/Textures/TextureSpliter.cs
+++ FreeMote.Psb/Textures/TextureSpliter.cs
@@ -5,6 +5,7 @@ using System.Drawing.Imaging;
 using System.IO;
 #if USE_FASTBITMAP
 using FastBitmapLib;
+
 #else
 using System.Drawing.Drawing2D;
 #endif
@@ -14,29 +15,30 @@ namespace FreeMote.Psb.Textures
     public static class TextureSpliter
     {
         /// <summary>
-        /// <see cref="PsbResCollector.CollectResources"/> for packed-texture specs, like <see cref="PsbSpec.win"/>
+        /// <see cref="PsbResHelper.CollectResources"/> for packed-texture specs, like <see cref="PsbSpec.win"/>
         /// </summary>
         /// <param name="psb"></param>
         /// <returns></returns>
         public static List<ImageMetadata> CollectSplitResources(this PSB psb)
         {
             List<ImageMetadata> resList = new List<ImageMetadata>();
-            var source = (PsbDictionary)psb.Objects["source"];
+            var source = (PsbDictionary) psb.Objects["source"];
             foreach (var tex in source)
             {
                 if (tex.Value is PsbDictionary texDic)
                 {
                     var typeStr = (PsbString) texDic["texture"].Children("type");
+                    typeStr = PsbPixelFormat.WinRGBA8.ToStringForPsb().ToPsbString();
                     var bmps = SplitTexture(texDic, psb.Platform);
-                    var icons = (PsbDictionary)texDic["icon"];
+                    var icons = (PsbDictionary) texDic["icon"];
                     foreach (var iconPair in icons)
                     {
-                        var res = new PsbResource()
+                        var res = new PsbResource
                         {
-                            Data = 
+                            Data =
                                 RL.GetPixelBytesFromImage(bmps[iconPair.Key], typeStr.Value.ToPsbPixelFormat(psb.Platform))
                         };
-                        var icon = (PsbDictionary)iconPair.Value;
+                        var icon = (PsbDictionary) iconPair.Value;
                         var md = PsbResHelper.GenerateImageMetadata(icon, res);
                         md.Spec = psb.Platform;
                         md.TypeString = typeStr;
@@ -50,8 +52,8 @@ namespace FreeMote.Psb.Textures
 
         public static Dictionary<string, Bitmap> SplitTexture(PsbDictionary tex, PsbSpec spec)
         {
-            var icon = (PsbDictionary)tex["icon"];
-            var texture = (PsbDictionary)tex["texture"];
+            var icon = (PsbDictionary) tex["icon"];
+            var texture = (PsbDictionary) tex["texture"];
 
             //var mipmap = (PsbDictionary)texture["mipMap"]; //TODO: Mipmap?
             Dictionary<string, Bitmap> textures = new Dictionary<string, Bitmap>(icon.Count);
@@ -59,7 +61,8 @@ namespace FreeMote.Psb.Textures
             var pixel = texture[Consts.ResourceKey];
             if (pixel == null || !(pixel is PsbResource pixelRes))
             {
-                throw new PsbBadFormatException(PsbBadFormatReason.Resources, "External Texture PSB is not supported. Please Link textures into PSB.");
+                throw new PsbBadFormatException(PsbBadFormatReason.Resources,
+                    "External Texture PSB is not supported. Please Link textures into PSB.");
                 return textures;
             }
 
@@ -68,11 +71,11 @@ namespace FreeMote.Psb.Textures
             Bitmap bmp = md.ToImage();
             foreach (var iconPair in icon)
             {
-                var info = (PsbDictionary)iconPair.Value;
-                var width = (int)(PsbNumber)info["width"];
-                var height = (int)(PsbNumber)info["height"];
-                var top = (int)(PsbNumber)info["top"];
-                var left = (int)(PsbNumber)info["left"];
+                var info = (PsbDictionary) iconPair.Value;
+                var width = (int) (PsbNumber) info["width"];
+                var height = (int) (PsbNumber) info["height"];
+                var top = (int) (PsbNumber) info["top"];
+                var left = (int) (PsbNumber) info["left"];
                 Bitmap b = new Bitmap(width, height, PixelFormat.Format32bppArgb);
 #if USE_FASTBITMAP
                 using (FastBitmap f = b.FastLock())
@@ -89,6 +92,7 @@ namespace FreeMote.Psb.Textures
 #endif
                 textures.Add(iconPair.Key, b);
             }
+
             bmp.Dispose();
             return textures;
         }
@@ -101,33 +105,38 @@ namespace FreeMote.Psb.Textures
         /// <param name="option">Save option</param>
         /// <param name="imageFormat">Save format</param>
         /// <param name="pixelFormat">When save to PSB special formats, specific pixel format to use</param>
-        public static void SplitTextureToFiles(this PSB psb, string path, PsbExtractOption option = PsbExtractOption.Extract, PsbImageFormat imageFormat = PsbImageFormat.png, PsbPixelFormat pixelFormat = PsbPixelFormat.None)
+        public static void SplitTextureToFiles(this PSB psb, string path, PsbExtractOption option = PsbExtractOption.Extract,
+            PsbImageFormat imageFormat = PsbImageFormat.png, PsbPixelFormat pixelFormat = PsbPixelFormat.None)
         {
             if (!Directory.Exists(path))
             {
                 Directory.CreateDirectory(path);
             }
-            var source = (PsbDictionary)psb.Objects["source"];
+
+            var source = (PsbDictionary) psb.Objects["source"];
             foreach (var texPair in source)
             {
                 if (!(texPair.Value is PsbDictionary tex))
                 {
                     continue;
                 }
+
                 var name = texPair.Key;
                 if (!Directory.Exists(Path.Combine(path, name)))
                 {
                     Directory.CreateDirectory(Path.Combine(path, name));
                 }
-                var icon = (PsbDictionary)tex["icon"];
-                var texture = (PsbDictionary)tex["texture"];
+
+                var icon = (PsbDictionary) tex["icon"];
+                var texture = (PsbDictionary) tex["texture"];
 
                 //var mipmap = (PsbDictionary)texture["mipMap"]; //TODO: Mipmap?
 
                 var pixel = texture[Consts.ResourceKey];
                 if (pixel == null || !(pixel is PsbResource pixelRes))
                 {
-                    throw new PsbBadFormatException(PsbBadFormatReason.Resources, "External Texture PSB is not supported. Please Link textures into PSB.");
+                    throw new PsbBadFormatException(PsbBadFormatReason.Resources,
+                        "External Texture PSB is not supported. Please Link textures into PSB.");
                     return;
                 }
 
@@ -138,12 +147,12 @@ namespace FreeMote.Psb.Textures
                 foreach (var iconPair in icon)
                 {
                     var savePath = Path.Combine(path, name, iconPair.Key);
-                    var info = (PsbDictionary)iconPair.Value;
-                    var width = (int)(PsbNumber)info["width"];
-                    var height = (int)(PsbNumber)info["height"];
-                    var top = (int)(PsbNumber)info["top"];
-                    var left = (int)(PsbNumber)info["left"];
-                    var attr = (int)(PsbNumber)info["attr"];
+                    var info = (PsbDictionary) iconPair.Value;
+                    var width = (int) (PsbNumber) info["width"];
+                    var height = (int) (PsbNumber) info["height"];
+                    var top = (int) (PsbNumber) info["top"];
+                    var left = (int) (PsbNumber) info["left"];
+                    var attr = (int) (PsbNumber) info["attr"];
                     Bitmap b = new Bitmap(width, height, PixelFormat.Format32bppArgb);
 #if USE_FASTBITMAP
                     using (FastBitmap f = b.FastLock())
@@ -181,14 +190,13 @@ namespace FreeMote.Psb.Textures
                                     //b.Save(savePath + $"_{attr}.png", ImageFormat.png);
                                     break;
                             }
+
                             break;
                     }
                 }
+
                 bmp.Dispose();
             }
         }
     }
 }
-
-
-
