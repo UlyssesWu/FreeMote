ee8ae0dd037ca48e6ada645a43e4e648af4857db
UlyssesWu <wdwxy12345@gmail.com>
2021-04-19 19:22:24 +0800

    only skip adding suffix for `.m` files #55

--- FreeMote.Psb/PsbExtension.cs
+++ FreeMote.Psb/PsbExtension.cs
@@ -943,7 +943,7 @@ namespace FreeMote.Psb
         /// <returns></returns>
         public static string ArchiveInfoGetFileNameAppendSuffix(string name, string suffix)
         {
-            if (!name.Contains("."))
+            if (!name.EndsWith(".m")) //if a file name ends with .m, it's a naughty bad file with its own suffix
             {
                 return name + suffix;
             }
