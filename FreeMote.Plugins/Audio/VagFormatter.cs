9d61ba22ad65deea949c027bf0e2d9f5f2f1e4b6
Ulysses Wu <wdwxy12345@gmail.com>
2024-01-07 14:30:16 +0800

    update readme

--- FreeMote.Plugins/Audio/VagFormatter.cs
+++ FreeMote.Plugins/Audio/VagFormatter.cs
@@ -58,7 +58,7 @@ namespace FreeMote.Plugins.Audio
 
         public byte[] ToWave(AudioMetadata md, IArchData archData, string fileName = null, Dictionary<string, object> context = null)
         {
-            bool hasTool = File.Exists(ToolPath);
+            //bool hasTool = File.Exists(ToolPath);
 
             VagFile vag = new VagFile();
             if (vag.LoadFromStream(new MemoryStream(archData.Data.Data)))
