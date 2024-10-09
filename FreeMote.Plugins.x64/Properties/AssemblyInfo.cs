322ad91233161bbab08a58c3812b3beea69254f2
Ulysses Wu <wdwxy12345@gmail.com>
2024-10-10 01:16:49 +0800

    lift version

--- FreeMote.Plugins.x64/Properties/AssemblyInfo.cs
+++ FreeMote.Plugins.x64/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("7b5bae81-6779-4404-9e6e-512be4471ab9")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.8.0.0")]
-[assembly: AssemblyFileVersion("3.8.0.0")]
+[assembly: AssemblyVersion("3.9.0.0")]
+[assembly: AssemblyFileVersion("3.9.0.0")]
